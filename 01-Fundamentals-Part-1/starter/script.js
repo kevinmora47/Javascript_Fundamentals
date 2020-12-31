eeb5f8a30c1e198d1c5d71e0bc0891d13418eaf9
kevinmora47 <kevinmora47@gmail.com>
2020-12-31 15:17:22 -0600

    Let, Const, Var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,22 @@
-let js = "amazing";
-if (js === "amazing") {
-  alert("Javascript is amazing!");
-}
+//  Boolean value.
+let javascriptIsfun = true;
+console.log(javascriptIsfun);
+// Type of.
+console.log(typeof javascriptIsfun);
 
-console.log(40 + 8 + 23 - 10); 
+// Dynamic typing.
+
+javascriptIsfun = "Yes!";
+console.log(typeof javascriptIsfun);
+
+// Undefined value.
+let year;
+console.log(year);
+console.log(typeof year);
+
+// Const value.
+
+const birthYear = 1999;
+// birthYear = 2000;
+console.log(birthYear);
+
