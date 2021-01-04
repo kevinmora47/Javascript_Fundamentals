85b3aedfba86d744f42e7cf37224483fa7133e58
kevinmora47 <kevinmora47@gmail.com>
2021-01-04 08:45:41 -0600

    Template Strings

--- 01-Fundamentals-Part-1/starter/templateStrings.js
+++ 01-Fundamentals-Part-1/starter/templateStrings.js
@@ -1,1 +1,15 @@
-const fir;
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2020;
+
+// Old way
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old" + job + "!";
+console.log(jonas);
+
+//   Template literal
+const jonasLiteral = `I'm ${firstName}, a ${
+  year - birthYear
+} years old ${job}!`;
+console.log(jonasLiteral);
