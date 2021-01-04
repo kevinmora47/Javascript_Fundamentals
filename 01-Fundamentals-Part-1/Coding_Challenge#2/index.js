6ffd176df3ccf6b3b88640866986b824d9f4a9b7
kevinmora47 <kevinmora47@gmail.com>
2021-01-04 09:15:31 -0600

    Coding Challenge #2

---
+++ 01-Fundamentals-Part-1/Coding_Challenge#2/index.js
@@ -0,0 +1,15 @@
+const jonhBMI = 23.9;
+const markBMI = 28.3;
+let result;
+
+if (jonhBMI > markBMI) {
+  result = `Johns BMI is higher than Mak's BMI:
+    Marks BMI result: ${markBMI} 🏋️‍♀️
+    John's BMI result: ${jonhBMI} 🏋️‍♀️`;
+  console.log(result);
+} else {
+  result = `Mark's BMI is higher than John's BMI:
+    Marks BMI result: ${markBMI} 🏋️‍♀️
+    John's BMI result: ${jonhBMI} 🏋️‍♀️`;
+  console.log(result);
+}
