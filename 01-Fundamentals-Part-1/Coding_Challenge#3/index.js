7c72a650c6b527189cdac4dfd690cdf8deaeb9f6
kevinmora47 <kevinmora47@gmail.com>
2021-01-05 16:08:50 -0600

    Coding Challenge #3

--- 01-Fundamentals-Part-1/Coding_Challenge#3/index.js
+++ 01-Fundamentals-Part-1/Coding_Challenge#3/index.js
@@ -1,1 +1,17 @@
-alert("hey");
+const dolphinTeam = (100, 108, 89) / 3;
+const koalasTeam = 88 + 91 + 110 / 3;
+const minimumScore = 100;
+
+if (dolphinTeam > koalasTeam && dolphinTeam >= 100) {
+  console.log("Dolphins won the trophy-⚡");
+} else if (koalasTeam > dolphinTeam && koalasTeam >= 100) {
+  console.log("Koalas won the trophy ⚡");
+} else if (
+  dolphinTeam === koalasTeam &&
+  dolphinTeam >= 100 &&
+  koalasTeam >= 100
+) {
+  console.log("Both with the trophy⚡");
+} else {
+  console.log("Nobody wins 😢😭");
+}
