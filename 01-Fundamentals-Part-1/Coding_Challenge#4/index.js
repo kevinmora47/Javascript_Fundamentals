6ca8d3abff8313f3ec2d507e33fd9a4a059e3e6f
kevinmora47 <kevinmora47@gmail.com>
2021-01-06 21:30:44 -0600

    Strict mode

---
+++ 01-Fundamentals-Part-1/Coding_Challenge#4/index.js
@@ -0,0 +1,13 @@
+const lowTip = 0.15;
+const highTip = 0.2;
+const bill = 40;
+
+bill > 50 && bill <= 300
+  ? console.log(
+      `Bill total: ${bill + bill * lowTip}$, Tip: ${
+        bill * lowTip
+      }$ Bill: ${bill}$`
+    )
+  : console.log(
+      `Bill total: ${bill + bill * highTip}, Tip: ${bill * highTip} Bill${bill}`
+    );
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+// Strict mode helps up to write better code.
+"use strict";
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicence = true;
+  if (hasDriversLicence) {
+    console.log("I can drive");
+  }
+}
+
+const interfaces = "Audio";
