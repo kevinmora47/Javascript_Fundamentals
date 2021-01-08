60fa4964c63ffff20ee6894a41a5cc1095b94aae
kevinmora47 <kevinmora47@gmail.com>
2021-01-07 22:30:16 -0600

    Arrays

---
+++ 02-Fundamentals-Part-2/arraysIntroduction/index.js
@@ -0,0 +1,15 @@
+// Literal array declaration.
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+
+// Array length
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+// Replacing array value.
+friends[2] = "Kevin";
+console.log(friends);
