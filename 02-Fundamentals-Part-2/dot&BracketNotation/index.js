ea5c006aceba41147d7b7dc1843ea0163a69773e
kevinmora47 <kevinmora47@gmail.com>
2021-01-08 23:54:48 -0600

    Chenges

---
+++ 02-Fundamentals-Part-2/dot&BracketNotation/index.js
@@ -0,0 +1,21 @@
+// Object literal expression.
+const kevin = {
+  firstName: "Kevin",
+  lastName: "Mora",
+  age: 21,
+  job: "teacher",
+  friends: ["Emily", "Sofia"],
+};
+// The order of an object is sorted alphabetically.
+
+// Dot Notation.
+console.log(kevin.lastName);
+
+// Bracket notation, useful example.
+const interestedIn = prompt(`What do you like to know about me?
+1-First name.
+2-Last name.
+3-Age.
+4.Job.
+5.Friends.`);
+console.log(jonas[interestedIn]); // This will display the property that the user typed.
