1595ffb274bf4ca5265edbbc9e886821f5b7efee
kevinmora47 <kevinmora47@gmail.com>
2021-01-10 12:45:30 -0600

    For loop

---
+++ 02-Fundamentals-Part-2/ForLoop/index.js
@@ -0,0 +1,14 @@
+// Without a loop.
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+console.log("Lifting weight");
+
+// For loop keeps running while condition is true.
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
